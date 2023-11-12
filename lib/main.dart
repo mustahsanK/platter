5f80fb004d587b541b0252e7aecbbada0d71bdfd
mustahsanK <mustahsan60@outlook.com>
2023-11-13 00:10:05 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PlatterApp());
+}
+
+class PlatterApp extends StatelessWidget {
+  const PlatterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
